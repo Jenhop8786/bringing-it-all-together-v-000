e15ef2b02d95d24e92da453b0998253d56f3160d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-04 17:58:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,22 +8,34 @@ class Dog
   end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
     )
-    SQL 
+    SQL
 
     DB[:conn].execute(sql)
-  end 
+  end
 
 def self.drop_table
-  sql = <<-SQL 
+  sql = <<-SQL
   DROP TABLE dogs
-  SQL 
+  SQL
 
   DB[:conn].execute(sql)
-end 
+end
 
+def save 
+  if self.id != nil
+    self.update
+  else 
+    sql = <<-SQL 
+    INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL 
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+end 
