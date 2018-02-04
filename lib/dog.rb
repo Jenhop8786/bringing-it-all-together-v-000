c36c84015ac213acfe18dca67eebb664cef44b0d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-04 17:53:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,23 @@ class Dog
     @id = nil
   end
 
-  
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL 
+
+    DB[:conn].execute(sql)
+  end 
+
+def self.drop_table
+  sql = <<-SQL 
+  DROP TABLE dogs
+  SQL 
+
+  DB[:conn].execute(sql)
+end 
+
